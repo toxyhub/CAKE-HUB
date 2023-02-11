f84c927ec269d437faa40391e6a298aabecc5c64
Aravind <aravindvichu3050@gmail.com>
2023-02-11 10:41:38 +0530

    cake product model was created but not yet migrated

--- product/models.py
+++ product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class CakePro(models.Model): #inheritance
+    name=models.CharField(max_length=200) #creating field system
+    price=models.IntegerField()
+    image=models.ImageField(upload_to='pic') #creating a folder and will put 
+    quanty=models.FloatField()
+    discount=models.IntegerField(default=0)
+    descript=models.TextField()
+    date=models.DateTimeField(auto_now_add=True) #takes the correct date
+    
