93ed37fbe1132a075b5672934698fa47d3f46697
Aravind <aravindvichu3050@gmail.com>
2023-02-24 10:33:47 +0530

    Session completed

--- product/views.py
+++ product/views.py
@@ -8,7 +8,30 @@ def product(request):
 
 def details(request):
     idm=request.GET['id']
-    data=CakePro.objects.get(id=idm)
+    data=CakePro.objects.get(id=idm)    #id is the database id compare with idm to take the product.
+    if 'recent_view' in request.session:
+        if idm in request.session['recent_view']:
+            request.session['recent_view'].remove(idm)
+        
+        if len (request.session['recent_view'])>4:
+            request.session['recent_view'].pop()
+        print('hi',request.session['recent_view'])
+        recent=[]
+        for i in request.session['recent_view']:
+            recent.append(CakePro.objects.get(id=i))
+        request.session ['recent_view'].insert(0,idm)
+        request.session.modified=True
+
+        return render(request,'product.html',{'prodetail':data,'recent':recent})
+        
+        #img1=CakePro.objects.filter(id__in=request.session['recent_view'])
+        #print('helloooo',img1)
+
+    else:
+        request.session['recent_view']=[idm]      #its the list method for creating empty list
+        request.session.modified=True
+
+    request.session.modified=True
     
     #return render(request,'post.html',{"prodetail":data})
     return render(request,'product.html',{"prodetail":data})
